e9883d6b8b296e8be6ff625d6232ed89dcfb2b96
krabike <krabike88@gmail.com>
2024-10-31 23:53:40 +0300

    timer

--- Code/commands/timer.py
+++ Code/commands/timer.py
@@ -10,8 +10,10 @@ class ClosedTimer:
             if message.from_user.id == 1040778722:
                 without_command = message.text.split(maxsplit=1)
                 get_number = float(without_command[1])
-                await message.answer(f'Timer `set` {get_number}h', parse_mode='Markdown')
-                await asyncio.sleep(get_number*3600)
-                await message.answer(f'*Timeout {get_number/3600}:*\ntake a `break` at least 20 minutes', parse_mode='Markdown')
+                number_rounded = round(get_number, 4)
+                number_in_hours = get_number*3600
+                await message.answer(f'Timer `set` {number_rounded}h', parse_mode='Markdown')
+                await asyncio.sleep(number_in_hours)
+                await message.answer(f'*Timeout {number_rounded}h:*\ntake a `break` at least 20 minutes', parse_mode='Markdown')
         except:
             await message.answer('*Установи время* для таймера `после команды`', parse_mode='Markdown')
